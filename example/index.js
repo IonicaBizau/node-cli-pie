bc4964df435231e222d41e5c05f0988155006ba5
Ionică Bizău <bizauionica@yahoo.com>
2015-05-05 07:29:16 +0300

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,10 @@
+var Pie = require("../lib");
+
+var p = new Pie(200, 200, [
+    { name: "Water", value: 70, color: "#3498db" }
+  , { name: "Land", value: 30, color: "#f1c40f" }
+], {
+    legend: true
+});
+
+console.log(p.toString());
