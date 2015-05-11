e897b684bfd2a4db1d5a798f6bd6ab87ce262c2a
Ionică Bizău <bizauionica@yahoo.com>
2015-05-11 10:24:50 +0300

    Generate unique colors

--- example/index.js
+++ example/index.js
@@ -1,8 +1,10 @@
 var Pie = require("../lib");
 
 var p = new Pie(30, [
-    { name: "Water", value: 70, color: "#3498db" }
-  , { name: "Land", value: 30, color: "#f1c40f" }
+    { name: "Water", value: 70 }
+  , { name: "Land", value: 30 }
+  , { name: "Land", value: 50 }
+  , { name: "Land", value: 10 }
 ], {
     legend: true
 });
