93ba863b201e532179b32b7ea12cab4c3099f831
Ionică Bizău <bizauionica@yahoo.com>
2015-05-22 08:17:25 +0300

    Label instead of name

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
 var Pie = require("../lib");
 
 var p = new Pie(5, [
-    { name: "Water", value: 70, color: [ 0, 0, 255] }
-  , { name: "Land", value: 30, color: [255, 240, 0] }
+    { label: "Water", value: 70, color: [ 0, 0, 255] }
+  , { label: "Land", value: 30, color: [255, 240, 0] }
 ], {
     legend: true
 });
