3720a9cb2804901c2e9a7b4480fe85a05b0ed9ef
Ionică Bizău <bizauionica@yahoo.com>
2015-05-24 17:03:16 +0300

    Comments

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
+// Dependencies
 var Pie = require("../lib");
 
+// Generate a new pie, with radius 5 characters
 var p = new Pie(5, [
     { label: "Water", value: 70, color: [ 0, 0, 255] }
   , { label: "Land", value: 30, color: [255, 240, 0] }
@@ -7,19 +9,24 @@ var p = new Pie(5, [
     legend: true
 });
 
+// Stringify
 console.log(p.toString());
-p.options.no_ansi = true;
 
+// Stringify, after disabling the ansi styles
+p.options.no_ansi = true;
 console.log(p.toString());
+
+// Add a new item
 p.add({
     label: "Test"
   , value: 20
 });
 
-p.options.no_ansi = true;
+// Enable the ansi styles
+p.options.no_ansi = false;
 console.log(p.toString());
 
-p.options.no_ansi = false;
+// Disable the ansi styles and reset colors
+p.options.no_ansi = true;
 p.colors = {};
-
 console.log(p.toString());
